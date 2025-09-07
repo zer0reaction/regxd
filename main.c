a3e7dadf9ad1353fd01c4044289479ca52b6c1d4
zer0reaction <boltgopnik@gmail.com>
2025-09-08 00:23:13 +0300

    Started simplifying things

--- main.c
+++ main.c
@@ -7,73 +7,6 @@
 #include "table.h"
 #include "compiled.h"
 
-typedef enum {
-    TOKEN_ERROR = 0,
-    TOKEN_CH,                   // [x] character (includes escaped ones)
-    TOKEN_SBR_OPEN,             // [x] [
-    TOKEN_SBR_CLOSE,            // [x] ]
-    TOKEN_SBR_CARET_OPEN,       // [x] [^
-    TOKEN_MINUS,                // [x] -
-    TOKEN_DOT,                  // [x] .
-} Token_Type;
-
-typedef struct {
-    Token_Type type;
-
-    struct {
-        char value;
-    } ch;
-} Token;
-
-void compile(Table *t, const char *str)
-{
-    assert(0 && "todo");
-    (void)t;
-    (void)str;
-
-    #if 0
-    uint32_t state = STATE_INIT;
-    bool in_cc = false;
-
-    while (str[0]) {
-        assert(state < ROWS_COUNT && "too many states");
-
-        if (str[0] == '[') {
-            in_cc = true;
-            str++;
-            continue;
-        }
-
-        if (str[0] == ']') {
-            assert(in_cc && "stray ']' found");
-            in_cc = false;
-            str++;
-            state++;
-            continue;
-        }
-
-        if (in_cc) {
-            t->data[state][(uint8_t)str[0]] = state + 1;
-            str++;
-            continue;
-        }
-
-        if (str[0] == '$') {
-            t->data[state][0] = state + 1;
-        } else {
-            t->data[state][(uint8_t)str[0]] = state + 1;
-        }
-
-        str++;
-        state++;
-    }
-
-    assert(!in_cc && "character class not closed");
-
-    t->rows = state - STATE_INIT + 1;
-    #endif
-}
-
 void dump(const Table *t)
 {
     size_t i, j;
@@ -94,10 +27,23 @@ void dump_compiled(const Table *t)
     printf("static const Table <compiled> = {\n");
     printf("    .rows = %u,\n", t->rows);
     printf("    .data = {\n");
+
     for (i = 0; i < t->rows; i++) {
-        printf("        { ");
+        printf("        {\n");
+        printf("            ");
+
         for (j = 0; j < EVENTS_COUNT; j++) {
-            printf("%u, ", t->data[i][j]);
+            if ((j + 1) % 16 == 0) {
+                printf("%d,", t->data[i][j]);
+            } else {
+                printf("%d, ", t->data[i][j]);
+            }
+
+            if ((j + 1) % 16 == 0) {
+                printf("\n");
+                if (j != EVENTS_COUNT - 1) printf("            ");
+                else                       printf("        ");
+            }
         }
         printf("},\n");
     }
@@ -108,14 +54,18 @@ void dump_compiled(const Table *t)
 size_t match(const Table *t, const char *str)
 {
     const char *str_start = str;
-    uint32_t state = STATE_INIT;
+    State state = STATE_INIT;
 
     while (1) {
-        if (state == STATE_FAIL || state >= t->rows) {
-            break;
-        }
+        bool b = false;
+
+        b = (b || state == STATE_FAIL);
+        b = (b || state >= (State)t->rows);
+        b = (b || state == STATE_NOCONSUME);
+        if (b) break;
+
         state = t->data[state][(uint8_t)str[0]];
-        str++;
+        if (state != STATE_NOCONSUME) str++;
     }
 
     if (state == STATE_FAIL) {
@@ -127,17 +77,5 @@ size_t match(const Table *t, const char *str)
 
 int main(void)
 {
-    #if 0
-    Table t = {0};
-
-    t.rows = 2;
-    t.data[1]['.'] = 2;
-
-    dump_compiled(&t);
-    #endif
-
-    const char *str = ".";
-    printf("%lu\n", match(&table_dot, str));
-
     return 0;
 }
--- table.h
+++ table.h
@@ -1,15 +1,20 @@
 #ifndef TABLE_H_
 #define TABLE_H_
 
+#include <stdint.h>
+
 #define ROWS_COUNT 32
 #define EVENTS_COUNT 128
 
-#define STATE_FAIL 0
-#define STATE_INIT 1
+#define STATE_FAIL      (State)0
+#define STATE_INIT      (State)1
+#define STATE_NOCONSUME (State)-1
+
+typedef int8_t State;
 
 typedef struct {
-    uint32_t rows;
-    uint32_t data[ROWS_COUNT][EVENTS_COUNT];
+    uint8_t rows;
+    State data[ROWS_COUNT][EVENTS_COUNT];
 } Table;
 
 #endif // TABLE_H_
